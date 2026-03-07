110b9684e0a889e8bcebc8b3d40edc5c9886f691
Andrea Capigiri <git@andreacapigiri.com>
2026-03-07 01:22:21 +0100

    Fix vari

--- ELO.DevTools/src/environments/environment.prod-chrome.ts
+++ ELO.DevTools/src/environments/environment.prod-chrome.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     destBrowser: 'Google Chrome',
-    favoriteEditorPath: 'chrome://bookmarks/',
-    QuinkUri: "https://andreacapigiri.com/quink?src=quink-ext"
+    favoriteEditorPath: 'chrome://bookmarks/'
 };
--- ELO.DevTools/src/environments/environment.prod-edge.ts
+++ ELO.DevTools/src/environments/environment.prod-edge.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     destBrowser: 'Microsoft Edge',
-    favoriteEditorPath: 'edge://favorites/',
-    QuinkUri: "https://andreacapigiri.com/quink?src=quink-ext"
+    favoriteEditorPath: 'edge://favorites/'
 };
